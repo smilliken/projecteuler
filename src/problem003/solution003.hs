dd177cf3ecc1aec5e2a6f14ab2e0d837c82c565a
Scott Milliken <scott@deltaex.com>
2012-12-29 01:08:37 -0800

    Simplified problem 3 haskell solution.

--- src/problem003/solution003.hs
+++ src/problem003/solution003.hs
@@ -1,6 +1,4 @@
 main = print . maximum . pfactors $ 600851475143
 pfactors 1 = []
-pfactors n = do
-    let pn = pfactor n
-    return pn ++ pfactors (div n pn)
+pfactors n = pfactor n : pfactors (div n (pfactor n))
 pfactor n = head . filter (\x -> mod n x == 0) $ [2..n]
