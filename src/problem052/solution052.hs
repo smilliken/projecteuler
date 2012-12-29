7f6f72b803ed1690c42c7a4994ffe95feb2531aa
Scott Milliken <scott@deltaex.com>
2012-12-28 19:54:06 -0800

    Updated import and minor style changes to problem 52 haskell solution.

--- src/problem052/solution052.hs
+++ src/problem052/solution052.hs
@@ -1,4 +1,4 @@
-import List
-main = putStrLn $ show $ head $ filter check [1..]
-digits x = sort $ map(\z -> read [z]::Int) (show $ x)
+import Data.List
+main = print . head . filter check $ [1..]
+digits = sort . map(\z -> read [z]::Int) . show
 check x = digits (x * 2) == digits (x * 3) && digits (x * 2) == digits (x * 4) && digits (x * 2) == digits (x * 5) && digits (x * 2) == digits (x * 6)
