c645a2fab70d19018d5eb0dc7128c5d0f2e72005
Scott Milliken <scott@deltaex.com>
2013-01-11 03:15:25 -0800

    Using simpler and faster Fibonacci generator in solution002.hs. Simplified rest of program.

--- src/problem002/solution002.hs
+++ src/problem002/solution002.hs
@@ -1,4 +1,2 @@
-main = print . sum . filter (\x -> mod x 2 == 0) . takeWhile (\x -> x <= 4000000) . map fib $ [1..]
-fib 1 = 1
-fib 2 = 2
-fib x = fib (x - 1) + fib (x - 2)
+main = print . sum . filter even . takeWhile (<=4000000) $ fibs
+fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
