047f76343a9514a3459528a84d4f85f7217d46cc
Scott Milliken <scott@deltaex.com>
2013-04-05 15:43:28 -0700

    Fixed bug in problem 50 python solution with prime checker.

--- src/problem050/solution050.py
+++ src/problem050/solution050.py
@@ -2,7 +2,7 @@ import math
 
 
 def is_prime(n):
-    return not any(a for a in xrange(2, int(math.sqrt(n))) if n % a == 0)
+    return not any(a for a in xrange(2, int(math.sqrt(n)) + 1) if n % a == 0)
 
 
 def main():
