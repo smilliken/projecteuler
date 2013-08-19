11cc914c56034553006770c85f66a62bbd198d1e
Scott Milliken <scott@deltaex.com>
2013-08-19 02:30:59 +0000

    Simplified solution to problem 23 in python.

--- src/problem023/solution023.py
+++ src/problem023/solution023.py
@@ -1,17 +1,14 @@
-import itertools
-
 def proper_divisors(n):
-    for i in xrange(1, n/2 + 1):
+    for i in xrange(1, n / 2 + 1):
         if n % i == 0:
             yield i
 
+
 def main():
     ceiling = 28123
-    proper_divisor_sums = [sum(proper_divisors(i)) for i in xrange(ceiling)]
-    abundant_nums = [i for i, x in enumerate(proper_divisor_sums) if x > i]
-    abundant_sums = set((a + b for a, b in itertools.product(abundant_nums, abundant_nums) if a + b <= ceiling))
-    holes = set(range(1, ceiling)).difference(abundant_sums)
-    print(sum(holes))
+    abundant_nums = [i for i in xrange(2, ceiling) if i < sum(proper_divisors(i))]
+    abundant_sums = set((a + b for a in abundant_nums for b in abundant_nums if a + b < ceiling))
+    print(sum(set(range(1, ceiling)).difference(abundant_sums)))
 
 if __name__ == '__main__':
     main()
