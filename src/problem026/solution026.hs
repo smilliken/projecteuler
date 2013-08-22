e03dd6ad77823bab8d4ed7754916c3319c089236
Scott Milliken <scott@deltaex.com>
2013-08-22 19:28:17 +0000

    Added solution to problem 26 in haskell.

--- src/problem026/solution026.hs
+++ src/problem026/solution026.hs
@@ -1,11 +1,6 @@
 main = print $ d
-    where (len, d) = maximum . map (\d -> (fractionPeriod d, d)) $ [2..999]
-fractionPeriod d = head [i2 - i1 |
-    (i1, r1) <- divr d, (i2, r2) <- take d . drop i1 $ divr d,
-    i1 < i2, r1 == r2]
+    where (len, d) = maximum . map (\d -> (fracPeriod d, d)) $ [2..999]
+fracPeriod d =
+    head [i2 - i1 | (i1, r1) <- divr d, (i2, r2) <- take d . drop i1 $ divr d, i1 < i2, r1 == r2]
 divr d = zip [1..] (divr' 1 d)
-divr' n d = (res, rem) : divr' rem d
-    where n' = n * 10
-          res = n' `div` d
-          rem = n' `mod` d
-
+divr' n d = let r = n * 10 `mod` d in r : divr' r d
