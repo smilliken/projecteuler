8505a9886025fbe799b4dcf2725ad6092e6dc8a0
Scott Milliken <scott@deltaex.com>
2013-09-28 08:48:10 +0000

    Simplified haskell solution to problem 27.

--- src/problem027/solution027.hs
+++ src/problem027/solution027.hs
@@ -1,10 +1,8 @@
-import Data.List (takeWhile)
-main = print $ a * b
-    where (l, a, b) = maximum
-            . map (\(a, b) -> (runLength isPrime (quadraticSeries a b), a, b))
-            $ [(a, b) | a<-[-999..999], b<-[-999..999]]
-quadraticSeries a b = map (\n -> n^2 + a * n + b) $ [0..]
-runLength p l = length . takeWhile p $ l
+import Data.List (takeWhile, foldl1')
+
+main = print . snd . foldl1' max . map (\(a, b) -> (quadPrimes a b, a * b)) $
+       [(a, b) | a<-[-999..999], b<-[-999..999]]
+quadPrimes a b = length . takeWhile isPrime . map (\n -> n^2 + a * n + b) $ [0..]
 isPrime n = elem n . takeWhile (<=n) $ primes
 primes = 2 : [x | x <- [3..],
     all (\p -> x `mod` p /= 0) . takeWhile (\p -> p^2 <= x) $ primes]
