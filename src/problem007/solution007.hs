4f6a1b220165102bd2da8e4faa1fe2236d2d0d15
Scott Milliken <scott@deltaex.com>
2013-09-29 01:33:24 +0000

    Simplified haskell solution to problem 7.

--- src/problem007/solution007.hs
+++ src/problem007/solution007.hs
@@ -1,2 +1,3 @@
-main = print . last . take 10001 $ primes
-primes = 2 : [x | x <- [3..], all (\p -> mod x p /= 0) . takeWhile (\p -> p^2 <= x) $ primes]
+main = print . head . drop 10000 $ primes
+primes = 2 : [x | x <- [3..],
+    all (\p -> mod x p /= 0) . takeWhile (\p -> p^2 <= x) $ primes]
