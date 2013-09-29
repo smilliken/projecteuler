bd563eae724a0ca8e86c713a1573705a4b7d6d69
Scott Milliken <scott@deltaex.com>
2013-09-29 02:20:25 +0000

    Simplified haskell solution to problem 15.

--- src/problem015/solution015.hs
+++ src/problem015/solution015.hs
@@ -1,3 +1,2 @@
-import Text.Printf (printf)
-main = printf "%d\n" $ combinations 40 20
-combinations n k = ((product [1..n]) `div` (product [1..k] * product [1..(n - k)]))::Integer
+main = print $ combinations 40 20
+combinations n k = product [1..n] `div` (product [1..k] * product [1..(n - k)])
