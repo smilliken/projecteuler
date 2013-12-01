e39d6ea85f87280b14364aed4ce303409f566779
Scott Milliken <scott@deltaex.com>
2013-12-01 06:35:34 +0000

    Performance optimizations to problem 14 solution in haskell.

--- src/problem014/solution014.hs
+++ src/problem014/solution014.hs
@@ -1,7 +1,9 @@
+import Data.Word (Word32)
 import Data.List (foldl1')
 import Data.Ord (comparing)
 
-collatz 1 = [1]
-collatz n = n : collatz (if even n then n `div` 2 else 3 * n + 1)
+collatzOdd :: Word32 -> [Word32]
+collatzOdd 1 = [1]
+collatzOdd n = n : collatzOdd ((if even n then n else 3 * n + 1) `div` 2)
 maximumBy' cmp = foldl1' (\a b -> case cmp a b of GT ->  a; _ -> b)
-main = print . head . maximumBy' (comparing length) . map collatz $ [1..1000000]
+main = print . head . maximumBy' (comparing length) . map collatzOdd $ [1..1000000]
